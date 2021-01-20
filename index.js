2f57d3a1b9725220a8e0753551a8e9a663fef3a0
Andrey <tatarinov02.andrey@gmail.com>
2021-01-20 14:54:54 +0400

    third task

--- index.js
+++ index.js
@@ -35,4 +35,15 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-}
+};
+
+// №3
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
